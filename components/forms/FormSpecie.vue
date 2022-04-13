22928865ff7d25432cf5708a38d66195c5c17e8d
owalid <othmane.el.ayadi@gmail.com>
2022-04-13 18:47:58 +0200

    add search

--- components/forms/FormSpecie.vue
+++ components/forms/FormSpecie.vue
@@ -3,8 +3,12 @@
     <v-row align="center" justify="center">
       <h1 class="primary--text">Sélection des légumes</h1>
     </v-row>
+    <!-- <pre>
+      {{ dataSpeciesFiltered }}
+    </pre> -->
     <v-row class="my-4">
       <v-text-field
+        v-model="textSearch"
         label="Recherche"
         append-icon="fas fa-magnifying-glass"
       >
@@ -14,12 +18,29 @@
       </v-text-field>
     </v-row>
     <v-row>
-      <v-col v-for="(dataSpecie, indexSpecie) in dataSpecies" :key="indexSpecie" cols="3" >
-        <div class="container_specie">
-          <img :src="dataSpecie.src_img" width="200px" height="150px"/>
+      <v-col
+        v-for="(dataSpecie, indexSpecie) in dataSpeciesFiltered"
+        :key="indexSpecie"
+        cols="4"
+        md="3"
+      >
+      <v-row justify="center" class="my-2">
+        <div
+          class="container-specie"
+          :class="{'container-specie-hover': indexSpecie === currentOver}"
+          @mouseenter="hoverSpecie(indexSpecie)"
+          @mouseleave="unhoverSpecie"
+        >
+          <img class="img_specie" :src="dataSpecie.src_img" />
           <p class="my-2 text-center">{{ dataSpecie.specie }}</p>
         </div>
+        </v-row>
       </v-col>
+    </v-row>
+    <v-row align="end" justify="end" class="mt-7">
+      <v-btn color="primary">
+        Suivant
+      </v-btn>
     </v-row>
   </v-container>
 </template>
@@ -30,16 +51,73 @@ export default {
   name: "FormSpecie",
   data() {
     return {
-      dataSpecies
+      dataSpecies,
+      textSearch: '',
+      currentOver: -1
+    }
+  },
+  computed: {
+    dataSpeciesFiltered() {
+      if (!this.textSearch) {
+        return this.dataSpecies
+      }
+      const result = this.dataSpecies.filter(specie => {
+        return specie.specie.toLowerCase().includes(this.textSearch.toLowerCase()) || specie.types.find(type => {
+          return type.complete_name.toLowerCase().includes(this.textSearch.toLowerCase()) || type.description.toLowerCase().includes(this.textSearch.toLowerCase())
+        })
+      })
+
+      return result;
+    }
+  },
+  methods: {
+    hoverSpecie(indexSpecie) {
+      this.currentOver = indexSpecie
+    },
+    unhoverSpecie() {
+      this.currentOver = -1
     }
   }
 }
 </script>
-<style scoped>
-.container_specie {
+<style scoped lang="scss">
+.container-specie {
+  &-hover {
+    opacity: 0.7;
+  }
+  cursor: pointer;
   border-radius: 12px;
-  width: 200px;
-  height: 200px;
+  width: 150px;
+  height: 150px;
   background: #F3F3F3;
+
+  .img_specie {
+    width: 150px;
+    height: 100px; 
+  }
+}
+
+@media (max-width: 425px) {
+  .container-specie {
+    width: 100px;
+    height: 100px;
+
+    .img_specie {
+      width: 100px;
+      height: 50px; 
+    }
+  }
+}
+
+@media (min-width: 768px) {
+  .container-specie {
+    width: 200px;
+    height: 200px;
+
+    .img_specie {
+      width: 200px;
+      height: 150px; 
+    }
+  }
 }
 </style>
